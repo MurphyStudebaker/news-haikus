7ec8f254aac4efce83579863f5cf49f9c1b63f08
MurphyStudebaker <murphy.stude@gmail.com>
2018-04-11 19:23:28 -0700

    Use pyhyphen and nltk to separate words and syllables

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # news-haikus
+This is a project that uses the Google News API and linguistic processing libraries
+to turn news headlines and articles into haikus that capture the media in a new light.
+
+#Tools Used
+The python library for the Google News API
+The PyHyphen package
--- newsHaikus.py
+++ newsHaikus.py
@@ -2,15 +2,32 @@
 #Author: Murphy Studebaker
 
 from newsapi import NewsApiClient
+from hyphen import Hyphenator
+from nltk.tokenize import word_tokenize
 import config
 
 #API access setup
 key = config.API_KEY
 news_api = NewsApiClient(api_key=key)
 
+#Language processing set up
+hyphen = Hyphenator('en_US')
+
 top_articles_data = news_api.get_top_headlines(language='en', country='us')
 articlesFound = top_articles_data['totalResults']
 
-for article in top_articles_data['articles']:
-    print ("ARTICLE: ")
-    print (article['title'])
+keyword = input("Enter a news topic: ")
+keyword_data = news_api.get_top_headlines(q=keyword,
+                                       language='en')
+
+#print syllables if data fetch was successful
+if (keyword_data['status'] == 'ok'):
+    headline = keyword_data['articles'][0]['title']
+    words = word_tokenize(headline)
+
+    for word in words:
+        syllables = hyphen.syllables(word)
+        for s in syllables:
+            print (s)
+else:
+    print ("Error fetching data.")
