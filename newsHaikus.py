7fec79a88dc831f64481135c231c00f88c3f9ef0
MurphyStudebaker <murphy.stude@gmail.com>
2018-04-16 14:10:38 -0700

    Function to set up each word in description for selection

--- newsHaikus.py
+++ newsHaikus.py
@@ -3,7 +3,7 @@
 
 from newsapi import NewsApiClient
 from hyphen import Hyphenator
-from nltk.tokenize import word_tokenize
+import nltk
 import config
 
 #API access setup
@@ -16,18 +16,25 @@ hyphen = Hyphenator('en_US')
 top_articles_data = news_api.get_top_headlines(language='en', country='us')
 articlesFound = top_articles_data['totalResults']
 
-keyword = input("Enter a news topic: ")
-keyword_data = news_api.get_top_headlines(q=keyword,
-                                       language='en')
-
+#keyword = input("Enter a news topic: ")
+#keyword_data = news_api.get_top_headlines(q=keyword,
+#                                       language='en')
+processed = []
 #print syllables if data fetch was successful
-if (keyword_data['status'] == 'ok'):
-    headline = keyword_data['articles'][0]['title']
-    words = word_tokenize(headline)
+def process(data, index):
+    if (data['status'] == 'ok'):
+        articles = data['articles']
+        description = articles[index]['description']
+        words = nltk.word_tokenize(description)
+        tags = nltk.pos_tag(words)
+        for word in tags:
+            length = len(hyphen.syllables(word[0]))
+            if (length == 0):
+                length = 1 #adjust for bug
+            entry = word + (length,)
+            processed.append(entry)
 
-    for word in words:
-        syllables = hyphen.syllables(word)
-        for s in syllables:
-            print (s)
-else:
-    print ("Error fetching data.")
+    else:
+        print ("Error fetching data.")
+
+process(top_articles_data, 10)
