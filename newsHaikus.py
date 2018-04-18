9f707bd95bcc3cd9698e73e40a2720593f6a119e
MurphyStudebaker <murphy.stude@gmail.com>
2018-04-17 20:29:52 -0700

    Main menu added and error check for keyword entry

--- newsHaikus.py
+++ newsHaikus.py
@@ -13,30 +13,52 @@ news_api = NewsApiClient(api_key=key)
 #Language processing set up
 hyphen = Hyphenator('en_US')
 
-top_articles_data = news_api.get_top_headlines(language='en', country='us')
-articlesFound = top_articles_data['totalResults']
+top_articles_data = []
+keyword_data = []
 
-#keyword = input("Enter a news topic: ")
-#keyword_data = news_api.get_top_headlines(q=keyword,
-#                                       language='en')
-#print syllables if data fetch was successful
+line_1 = ["NNP","JJ","NN","NN","NN"]
+line_2 = ["VB", "NNP", "RB", "NN","NN","NN","CC"]
+line_3 = ["VB", "CC", "JJ", "VB","VB","VB"]
+pattern = [line_1, line_2, line_3]
+
+def init():
+    print ("~ ~ ~ ~ H A I K U ~ T H E ~ N E W S ~ ~ ~ ~\n")
+    print ("1. Top Articles Today")
+    print ("2. Top Articles for my Keyword")
+    selection = input("Select: ")
+    print (selection)
+    if (selection == "1"):
+        top_articles_data = news_api.get_top_headlines(language='en', country='us')
+        if (top_articles_data["status"] == "ok"):
+            processed = processArticle(top_articles_data, 0)
+            writeHaiku(pattern, processed)
+        else:
+            print ("Rain, ice, gloom, and fog \n because a fatal error \n occurred with data")
+    elif (selection == "2"):
+        keyword = input("Enter a news topic: ")
+        keyword_data = news_api.get_top_headlines(q=keyword,
+                                               language='en')
+        if (keyword_data["status"] == "ok" and keyword_data["totalResults"] > 3):
+            processed = processArticle(keyword_data, 0)
+            writeHaiku(pattern, processed)
+        else:
+            print ("Rain, ice, gloom, and fog \n because a fatal error \n occurred with data")
+    else:
+        print ("My condolances, \n but your selection is wrong \n please attempt once more")
+
 def processArticle(data, index):
     processed = []
-    if (data['status'] == 'ok'):
-        articles = data['articles']
-        description = articles[index]['description']
-        words = nltk.word_tokenize(description)
-        tags = nltk.pos_tag(words)
-        for word in tags:
-            length = len(hyphen.syllables(word[0]))
-            if (length == 0):
-                length = 1 #adjust for bug
-            entry = word + (length,)
-            processed.append(entry)
-        return processed
-
-    else:
-        print ("Error fetching data.")
+    articles = data['articles']
+    description = articles[index]['description']
+    words = nltk.word_tokenize(description)
+    tags = nltk.pos_tag(words)
+    for word in tags:
+        length = len(hyphen.syllables(word[0]))
+        if (length == 0):
+            length = 1 #adjust for bug
+        entry = word + (length,)
+        processed.append(entry)
+    return processed
 
 def write_line(max_syllables, speech_pattern, processed_array): #selects each word
     remaining = max_syllables
@@ -59,12 +81,10 @@ def write_line(max_syllables, speech_pattern, processed_array): #selects each wo
             pattern_index = 0
     return line
 
-line_1 = ["NNP","JJ","NN","NN","NN"]
-line_2 = ["VB", "NNP", "RB", "NN","NN","NN","CC"]
-line_3 = ["VB", "CC", "JJ", "VB","VB","VB"]
-processed = processArticle(top_articles_data, 6)
-print (top_articles_data["articles"][6]["description"])
-print ("\n~ ~ ~ ~ H A I K U ~ T H E ~ N E W S ~ ~ ~ ~\n")
-print (write_line(5, line_1, processed))
-print (write_line(7, line_2, processed))
-print (write_line(5, line_3, processed))
+def writeHaiku(pattern, processed):
+    print ("\n~ ~ ~ ~ H A I K U ~ T H E ~ N E W S ~ ~ ~ ~\n")
+    print (write_line(5, pattern[0], processed))
+    print (write_line(7, pattern[1], processed))
+    print (write_line(5, pattern[2], processed))
+
+init()
